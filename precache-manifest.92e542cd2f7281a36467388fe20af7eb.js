6417fadf4af505905eb9bb144ffb34def28aa1a3
hacri <linleqi@gmail.com>
2019-03-16 15:01:29 +0800

    init

---
+++ precache-manifest.92e542cd2f7281a36467388fe20af7eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a663758822cca2db4eb3",
+    "url": "/static/js/main.f6734396.chunk.js"
+  },
+  {
+    "revision": "aa98ba5dc298f69df869",
+    "url": "/static/js/2.4b9bc6c2.chunk.js"
+  },
+  {
+    "revision": "a663758822cca2db4eb3",
+    "url": "/static/css/main.50a83d25.chunk.css"
+  },
+  {
+    "revision": "aa98ba5dc298f69df869",
+    "url": "/static/css/2.21d80a4a.chunk.css"
+  },
+  {
+    "revision": "3542e177a6b383e7bb4d89e442c5ad8e",
+    "url": "/index.html"
+  }
+];
